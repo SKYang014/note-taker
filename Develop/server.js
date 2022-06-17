d73e212a01c1824fc06ed7d043bd92f805617272
poofy <skyang014@gmail.com>
2022-06-17 16:43:47 -0700

    moved files around

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//middleware which instructs server to make certain files readily available 
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
